2a969cbd666178f38250b6ce0277412c9aa4737a
Angelina Mendoza Y <131885246+Antena19@users.noreply.github.com>
2023-05-24 22:47:49 -0400

    Update Juego de Azar.py
    Pude crear la lista :B

--- Juego de Azar.py
+++ Juego de Azar.py
@@ -6,6 +6,8 @@
 #Se generará 1 ronda
 #Si el jugador acierta a los números mostrará un mensaje de ganador.
 print("----Bienvenido a Lotín----");
-numGanadores=[]; #creo lista de 5 números ganadores
-for i in range(5):
+numPosibles = list(range(1,50)); #Creo la lista de los posibles números ganadores. Uso range para decir que quiero los números del 1 al 49. Uso list para transformar el range a lista.
+print(f" Números posibles: {numPosibles}"); #muestro los números posibles.
+listaConcursante=[]; #creo una lista vacía del concursante
+for i in range(5): #repito 5 veces...
     numConcursante=int(input("Ingrese un número: "));
