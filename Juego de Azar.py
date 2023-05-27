5a0185f7f51f33004288db14bafaa3a7bbeabebe
Angelina Mendoza Y <131885246+Antena19@users.noreply.github.com>
2023-05-27 12:44:45 -0400

    Update Juego de Azar.py

--- Juego de Azar.py
+++ Juego de Azar.py
@@ -1,4 +1,5 @@
 '''Juego de Azar'''
+import random
 #La empresa de juegos de azar “Lotín”, desea crea una aplicación móvil que genere sorteos rápidos dentro del mismo celular.
 #Para eso, le solicita crear un algoritmo que genere una lista de números de manera aleatoria donde si usted acierta, gana. Las reglas son las siguientes:
 #Los números participantes son del 1 al 49
